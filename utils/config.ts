1868b1fc72d796f8303a0128fa675f3285807cd9
Jaxcoder <codenamejason@outlook.com>
2024-03-26 15:13:00 -0400

    add abis

---
+++ utils/config.ts
@@ -0,0 +1,18 @@
+import { Address } from "viem";
+
+export const alloProxyAddress: Address =
+  "0x1133eA7Af70876e64665ecD07C0A0476d09465a1";
+
+export const registryProxyAddress: Address =
+  "0x4AAcca72145e1dF2aeC137E1f3C5E3D75DB8b5f3";
+
+export const strategyAddress: TStrategyAddress = {
+  "DirectGrants": "0x4AAcca72145e1dF2aeC137E1f3C5E3D75DB8b5f3",
+}
+
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "add your site url here";
+
+export type TStrategyAddress = {
+  [key: string]: Address;
+};
